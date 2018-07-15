3f6336156a03a883cb16ec08f2a9fee2a91027f2
DanyerRaf <41244222+DanyerRaf@users.noreply.github.com>
2018-07-15 15:47:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,19 +8,42 @@ client.user.setPresence( {
        status: "online",
        game: {
            name: "King|Server",
-           type: "JUGANDO"
+           type: "PLAYING"
        }
-   } );
+   });
+   
+   client.on("guildMemberAdd", (member) => {
+   console.log(`El PODEROSO ${member.user.username} se ha unido a ${member.guild.name}.`);
+   var canal = client.channels.get('123456789112455845');
+   canal.send(`Saludos ${member.user}, bienvenido al servidor, pasala bien.`);
+
+   });
 });
 var prefix = config.prefix;
 
-client.on("message", (message) => {
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "hola")) {
-    message.channel.send("Hola que tal?");
+client.on("message", (message) =>
+{
+ if (!message.content.startsWith(prefix)) return;
+ if (message.author.bot) return;
+
+ const args = message.content.slice(prefix.length).trim().split(/ +/g);
+ const command = args.shift().toLowerCase();
+
+  if(command === 'ban'){
+
+        let user = message.mentions.users.first();
+        let razon = args.slice(1).join(' ');
+
+        if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
+        if(!razon) return message.channel.send('Escriba un razón, `>x ban @username [razón]`');
+        if (!message.guild.member(user).bannable) return message.reply('No puedo banear al usuario mencionado.');
+
+
+        message.guild.member(user).ban(razon);
+        message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
+
   }
+
 });
 client.login(config.token);
 client.login(process.env.TOKEN);
