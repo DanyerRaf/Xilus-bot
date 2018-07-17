4be844d719d1fb969ed8924b466f9b1949001fdf
DanyerRaf <41244222+DanyerRaf@users.noreply.github.com>
2018-07-17 01:39:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,22 @@ client.on("message", (message) =>
         message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
 
   }
+   if (command === 'clear'){
+      message.delete (2100);
+
+   let messageCount = parseInt(args[0]);
+
+   if(!messageCount) return message.reply('Escriba la cantidad a eliminar')
+  .then(m => {m.delete(2150);});
+
+   if(messageCount >= 2 && messageCount <= 100){
+  message.channel.fetchMessages({limit: messageCount})
+  .then(messages => message.channel.bulkDelete(messages))
+  .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+   }else{
+    return message.reply('El limite a eliminar es de 100 mensajes.');
+  }
+}
 
 });
 client.login(config.token);
