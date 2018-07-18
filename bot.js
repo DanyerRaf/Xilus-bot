9a4322880aa3fe09057a7190fc38d8b10b8f1bf5
DanyerRaf <41244222+DanyerRaf@users.noreply.github.com>
2018-07-17 22:55:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,5 +61,28 @@ client.on("message", (message) =>
 }
 
 });
+
+if (command === 'invite'){
+  message.delete (2000);{
+  const embed = new Discord.RichEmbed()
+  .setTitle("Los Invito a mi Servidor.")
+  .setColor("#f7640e")
+  .setAuthor(message.author.username, message.author.avatarURL);
+  message.channel.send({embed});
+}
+
+var id = message.channel.id;
+message.guild.channels.get(id).createInvite({
+    maxAge: 0  //maxAge: 0 significa que el link sera permanente
+
+}).then(invite =>
+    message.channel.send("https://discord.gg/BmcaHBc")
+);
+/*
+NOTA: Tambien puede usar maxUse: 1 para el maximo uso del enlace, en vez de maxAge.
+
+*/
+};
+  
 client.login(config.token);
 client.login(process.env.TOKEN);
