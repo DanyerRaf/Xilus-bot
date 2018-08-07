446a5891364e2de543d7fa7cfba5a171cd8697c9
DanyerRaf <danyerbmx39@gmail.com>
2018-08-07 00:23:12 -0400

    #bot

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 const  client = new Discord.Client();
 const config = require("./config.json");
+const commands = require('./comandos/index.js');
+
+const COMMAND_GROUP = 'general';
+this.commands = comandos;
 
 client.on("ready", () => {
    console.log("Estoy listo!");
@@ -43,22 +47,7 @@ client.on("message", (message) =>
         message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
 
   }
-  if (command === 'clear'){
-     message.delete (2100);
 
-  let messageCount = parseInt(args[0]);
-
-  if(!messageCount) return message.reply('Escriba la cantidad a eliminar')
-  .then(m => {m.delete(2150);});
-
-  if(messageCount >= 2 && messageCount <= 100){
-  message.channel.fetchMessages({limit: messageCount})
-  .then(messages => message.channel.bulkDelete(messages))
-  .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-    } else{
-     return message.reply('El limite a eliminar es de 100 mensajes.');
-    }
-}
 if (command === 'invite'){
   message.delete (2000);{
   const embed = new Discord.RichEmbed()
