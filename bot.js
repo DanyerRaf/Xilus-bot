1c724a02acfb1eceb291c226c4a0eac0063841e2
DanyerRaf <danyerbmx39@gmail.com>
2018-08-07 01:19:53 -0400

    BOT principal commands

--- bot.js
+++ bot.js
@@ -2,10 +2,6 @@ const Discord = require("discord.js");
 const  client = new Discord.Client();
 const config = require("./config.json");
 
-
-const COMMAND_GROUP = 'general';
-this.commands = comandos;
-
 client.on("ready", () => {
    console.log("Estoy listo!");
 client.user.setPresence( {
@@ -18,7 +14,7 @@ client.user.setPresence( {
 
    client.on("guildMemberAdd", (member) => {
    console.log(`El PODEROSO ${member.user.username} se ha unido a ${member.guild.name}.`);
-   var canal = client.channels.get('421749579306762263');
+   var canal = client.channels.get('123456789112455845');
    canal.send(`Saludos ${member.user}, bienvenido al servidor, pasala bien.`);
 
    });
@@ -47,7 +43,22 @@ client.on("message", (message) =>
         message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
 
   }
+   if (command === 'clear'){
+      message.delete (2100);
 
+   let messageCount = parseInt(args[0]);
+
+   if(!messageCount) return message.reply('Escriba la cantidad a eliminar')
+  .then(m => {m.delete(2150);});
+
+   if(messageCount >= 2 && messageCount <= 100){
+  message.channel.fetchMessages({limit: messageCount})
+  .then(messages => message.channel.bulkDelete(messages))
+  .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+   }else{
+    return message.reply('El limite a eliminar es de 100 mensajes.');
+  }
+}
 if (command === 'invite'){
   message.delete (2000);{
   const embed = new Discord.RichEmbed()
