ae6ae351adc6b049f2a3a20bd1896a3d6855c8c4
DanyerRaf <danyerbmx39@gmail.com>
2018-08-09 11:36:48 -0400

    release

--- bot.js
+++ bot.js
@@ -131,20 +131,6 @@ message.channel.send({embed: {
    });
  }
 
-// Informacon acerca del bot
-if(command === "botinfo"){
-
-  let bicon = bot.user.displayAvatarURL;
-  let botembed = new Discord.RichEmbed()
-  .setDescription("Bot Information")
-  .setColor("#15f153")
-  .setThumbnail(bicon)
-  .addField("Bot Name", bot.user.username)
-  .addField("Created On", bot.user.createdAt);
-
-  return message.channel.send(botembed);
-}
-
 //
 
 });
