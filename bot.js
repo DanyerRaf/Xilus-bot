9fb0d9fbe12bade9895b776ea63af9db332bc419
DanyerRaf <danyerbmx39@gmail.com>
2018-08-09 17:22:12 -0400

    embed

--- bot.js
+++ bot.js
@@ -93,22 +93,37 @@ NOTA: Tambien puede usar maxUse: 1 para el maximo uso del enlace, en vez de maxA
 }
 if (command === 'help'){
 
-  .setTitle("Este es su título, puede contener 256 caracteres")
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor(0x00AE86)
-      .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
-      .setFooter("Pie de página, puede contener 2048 caracteres", client.user.avatarURL)
-      .setImage(message.author.avatarURL)
-      .setThumbnail(message.author.avatarURL)
-      .setTimestamp()
-      .setURL("http://discordlatam.com/documentacion-mybot")
-      .addField("Este es un título de campo, puede contener 256 caracteres",
-        "Este es un valor de campo, puede contener 2048 caracteres.")
-      .addField("Campo en línea", "Debajo del campo en línea", true)
-      .addBlankField(true)
-      .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
+  message.channel.send({embed: {
+      color: 3447003,
+      author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+      },
+      title: "Enlace Embed",
+      url: "http://discordlatam.com/documentacion-mybot",
+      description: "Mensaje de prueba para la descripcion del embed.",
+      fields: {
+          name: "Campo1",
+          value: "Pueden tener diferentes campos con pequeñas descripciones."
+      },
+      {
+          name: "Campo2",
+          value: "Puedes poner [Enlaces web](http://discordlatam.com/) dentro del embed."
+      },
+      {
+          name: "Campo3",
+          value: "Puedes poner todos los Markdown *cursiva* **__Marcado__** dentro de un embed."
+      }
+      ,
+      timestamp: new Date(),
+          footer: {
+              icon_url: client.user.avatarURL,
+              text: "discordlatam.com"
+          }
+      }
+  });
 
-  return message.channel.send(helpembed);
+
 }
 
 
