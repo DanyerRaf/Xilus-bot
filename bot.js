36794019c77fb4eb91a7c16d78973528fb7f57a9
DanyerRaf <danyerbmx39@gmail.com>
2018-08-09 12:56:14 -0400

    reconfig

--- bot.js
+++ bot.js
@@ -5,8 +5,13 @@ const  client = new Discord.Client();
 
 client.on("ready", () => {
    console.log("Estoy listo!");
-client.user.setActivity("Loading.."; {type: "PLAYING" })
-           
+client.user.setPresence( {
+       status: "online",
+       game: {
+           name: "League of Legends",
+           type: "WATCHING"
+       }
+   });
 
    client.on("guildMemberAdd", (member) => {
    console.log(`El PODEROSO ${member.user.username} se ha unido a ${member.guild.name}.`);
