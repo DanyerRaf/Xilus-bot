c79bdf47674b1bd01aee285b99117dc474549744
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 16:28:57 -0400

    invitar bot

--- commands/invitarbot.js
+++ commands/invitarbot.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 
-  message.channel.send({
+  {
     message.delete (2000);{
     const embed = new Discord.RichEmbed()
     .setTitle("Los Invito a mi Servidor.")
@@ -8,6 +8,6 @@ exports.run = (client, message, args) => {
     .setAuthor(message.author.username, message.author.avatarURL);
     message.channel.send({embed});
   }
-});
+};
 
 }
