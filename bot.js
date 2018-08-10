2b850bab47f1173ce01f3f6231b1ec3dcab8462b
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 15:13:05 -0400

    bot

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const  client = new Discord.Client();
+const  bot = new Discord.Client();
 const fs = require("fs");
 
 client.on("ready", async () => {
@@ -14,7 +14,7 @@ client.on("ready", async () => {
 
 var prefix = config.prefix;
 
-client.on("message", (message) =>
+bot.on("message", (message) =>
 {
  if (!message.content.startsWith(prefix)) return;
  if (message.author.bot) return;
@@ -23,12 +23,12 @@ client.on("message", (message) =>
  let args = message.content.slice(prefix.length).trim().split(/ +/g);
  let cmd = args.shift().toLowerCase();
 
- if (sender.client) return;
+ if (sender.bot) return;
  if (!message.content.startsWith(prefix)) return;
 
 try{
   let commandfile = require(`./commands/${cmd}.js`);
-  commandfile.run(client, message, args);
+  commandfile.run(bot, message, args);
 }catch(e) {
   console.log(e.message);
 }finally {
@@ -37,111 +37,111 @@ try{
 
  //comandos
 
-  if(command === 'ban'){
-
-        let user = message.mentions.users.first();
-        let razon = args.slice(1).join(' ');
-
-        if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
-        if(!razon) return message.channel.send('Escriba un razón, `>x ban @username [razón]`');
-        if (!message.guild.member(user).bannable) return message.reply('No puedo banear al usuario mencionado.');
-
-
-        message.guild.member(user).ban(razon);
-        message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
-
-  }
-   if (command === 'borrar'){
-      message.delete (2100);
-
-   let messageCount = parseInt(args[0]);
-
-   if(!messageCount) return message.reply('Escriba la cantidad a eliminar')
-  .then(m => {m.delete(2150);});
-
-   if(messageCount >= 2 && messageCount <= 100){
-  message.channel.fetchMessages({limit: messageCount})
-  .then(messages => message.channel.bulkDelete(messages))
-  .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-   }else{
-    return message.reply('El limite a eliminar es de 100 mensajes.');
-  }
-}
-if (command === 'invitarbot'){
-  message.delete (2000);{
-client.generateInvite(["ADMINISTRATOR","MANAGE_CHANNELS","MANAGE_ROLES",
-                      "MANAGE_MESSAGES","SEND_MESSAGES","CONNECT","BAN_MEMBERS"])
-.then(link =>{
-    message.channel.send('Link de invitación: '+ link);
-  });
-   }
- }
-
-if (command === 'invitar'){
-  message.delete (2000);{
-  const embed = new Discord.RichEmbed()
-  .setTitle("Los Invito a mi Servidor.")
-  .setColor("#f7640e")
-  .setAuthor(message.author.username, message.author.avatarURL);
-  message.channel.send({embed});
-}
-
-var id = message.channel.id;
-message.guild.channels.get(id).createInvite({
-    maxAge: 0  //maxAge: 0 significa que el link sera permanente
-
-}).then(invite =>
-    message.channel.send("https://discord.gg/BmcaHBc")
-);
-/*
-NOTA: Tambien puede usar maxUse: 1 para el maximo uso del enlace, en vez de maxAge.
-
-*/
-}
-if (command === 'ayuda'){
-
-  message.channel.send({
-
-      embed: {
-      color: 0xa4ff00,
-      author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-      },
-      title: "Enlace Embed",
-      description: "Mensaje de prueba para la descripcion del embed.",
-      fields: [{
-          name: "ban",
-          value: "```Banear a un usuario especificado.```"
-      },
-      {
-          name: "borrar",
-          value: "```Borrar cierta cantidad de publicaciones,requiere permisos.```"
-      },
-      {
-          name: "invitarbot",
-          value: "```Crea una invitacion del bot.```"
-      },
-      {
-          name: "invitar",
-          value: "```Crea una invitacion del servidor.```"
-      },
-      {
-          name: "ayuda",
-          value: "```Muestra los comandos que puedes utilizar.```"
-      }
-      ],
-      timestamp: new Date(),
-          footer: {
-              icon_url: client.user.avatarURL,
-              text: "Bot Oficial del sservidor `xilus Game`",
-          }
-      }
-  });
-}
+//   if(command === 'ban'){
+//
+//         let user = message.mentions.users.first();
+//         let razon = args.slice(1).join(' ');
+//
+//         if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
+//         if(!razon) return message.channel.send('Escriba un razón, `>x ban @username [razón]`');
+//         if (!message.guild.member(user).bannable) return message.reply('No puedo banear al usuario mencionado.');
+//
+//
+//         message.guild.member(user).ban(razon);
+//         message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
+//
+//   }
+//    if (command === 'borrar'){
+//       message.delete (2100);
+//
+//    let messageCount = parseInt(args[0]);
+//
+//    if(!messageCount) return message.reply('Escriba la cantidad a eliminar')
+//   .then(m => {m.delete(2150);});
+//
+//    if(messageCount >= 2 && messageCount <= 100){
+//   message.channel.fetchMessages({limit: messageCount})
+//   .then(messages => message.channel.bulkDelete(messages))
+//   .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+//    }else{
+//     return message.reply('El limite a eliminar es de 100 mensajes.');
+//   }
+// }
+// if (command === 'invitarbot'){
+//   message.delete (2000);{
+// client.generateInvite(["ADMINISTRATOR","MANAGE_CHANNELS","MANAGE_ROLES",
+//                       "MANAGE_MESSAGES","SEND_MESSAGES","CONNECT","BAN_MEMBERS"])
+// .then(link =>{
+//     message.channel.send('Link de invitación: '+ link);
+//   });
+//    }
+//  }
+//
+// if (command === 'invitar'){
+//   message.delete (2000);{
+//   const embed = new Discord.RichEmbed()
+//   .setTitle("Los Invito a mi Servidor.")
+//   .setColor("#f7640e")
+//   .setAuthor(message.author.username, message.author.avatarURL);
+//   message.channel.send({embed});
+// }
+//
+// var id = message.channel.id;
+// message.guild.channels.get(id).createInvite({
+//     maxAge: 0  //maxAge: 0 significa que el link sera permanente
+//
+// }).then(invite =>
+//     message.channel.send("https://discord.gg/BmcaHBc")
+// );
+// /*
+// NOTA: Tambien puede usar maxUse: 1 para el maximo uso del enlace, en vez de maxAge.
+//
+// */
+// }
+// if (command === 'ayuda'){
+//
+//   message.channel.send({
+//
+//       embed: {
+//       color: 0xa4ff00,
+//       author: {
+//           name: client.user.username,
+//           icon_url: client.user.avatarURL
+//       },
+//       title: "Enlace Embed",
+//       description: "Mensaje de prueba para la descripcion del embed.",
+//       fields: [{
+//           name: "ban",
+//           value: "```Banear a un usuario especificado.```"
+//       },
+//       {
+//           name: "borrar",
+//           value: "```Borrar cierta cantidad de publicaciones,requiere permisos.```"
+//       },
+//       {
+//           name: "invitarbot",
+//           value: "```Crea una invitacion del bot.```"
+//       },
+//       {
+//           name: "invitar",
+//           value: "```Crea una invitacion del servidor.```"
+//       },
+//       {
+//           name: "ayuda",
+//           value: "```Muestra los comandos que puedes utilizar.```"
+//       }
+//       ],
+//       timestamp: new Date(),
+//           footer: {
+//               icon_url: client.user.avatarURL,
+//               text: "Bot Oficial del sservidor `xilus Game`",
+//           }
+//       }
+//   });
+// }
 
 
 });
 
-client.login(config.token);
-client.login(process.env.TOKEN);
+bot.login(config.token);
+bot.login(process.env.TOKEN);
