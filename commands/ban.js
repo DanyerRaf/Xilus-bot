e52e0c14bbeeab5c6d2f1d9c0775b9226676d41d
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 16:55:37 -0400

    ban

--- commands/ban.js
+++ commands/ban.js
@@ -1,9 +1,9 @@
 exports.run = (bot, message, args) => {
 {
-  message.delete (2000);{
+  message.delete (2000);
           let user = message.mentions.users.first();
           let razon = args.slice(1).join(' ');
-
+          message.delete (2000);{
           if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
           if(!razon) return message.channel.send('Escriba un razón, `>x ban @username [razón]`');
           if (!message.guild.member(user).bannable) return message.reply('No puedo banear al usuario mencionado.');
@@ -12,6 +12,7 @@ exports.run = (bot, message, args) => {
           message.guild.member(user).ban(razon);
           message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
      }
+
     };
-message.delete (2000);
+
 }
