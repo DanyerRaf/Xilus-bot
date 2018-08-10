490c28406cc9a86fd05541953560eebe39464f0e
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 18:00:11 -0400

    ban

--- commands/invitarbot.js
+++ commands/invitarbot.js
@@ -1,15 +1,13 @@
 exports.run = (client, message, args) => {
 
-message.delete (2000);{
-  message.channel.send({
+  message.channel.send(message.delete (2000);{
     
-
     client.generateInvite(["ADMINISTRATOR","MANAGE_CHANNELS","MANAGE_ROLES",
                           "MANAGE_MESSAGES","SEND_MESSAGES","CONNECT","BAN_MEMBERS"])
     .then(link =>{
         message.channel.send('Link de invitación: '+ link);
       });
-    });
-  }
+
+   });
 
 }
