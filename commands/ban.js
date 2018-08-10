8e017b39cb0dd7a0d86f55373b73b1d4e96eb697
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 00:10:24 -0400

    1

--- commands/ban.js
+++ commands/ban.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
 
 module.exports.run = async (bot, message, args) => {
   let user = message.mentions.users.first();
