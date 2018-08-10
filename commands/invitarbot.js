ff538b87be2dd221d5e26e7d6ceb364b38ce1c24
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 16:26:59 -0400

    invitbot

---
+++ commands/invitarbot.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args) => {
+
+  message.channel.send({
+    message.delete (2000);{
+    const embed = new Discord.RichEmbed()
+    .setTitle("Los Invito a mi Servidor.")
+    .setColor("#f7640e")
+    .setAuthor(message.author.username, message.author.avatarURL);
+    message.channel.send({embed});
+  }
+});
+
+}
