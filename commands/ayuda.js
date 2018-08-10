5966c9bafd4f8ba9f7ec5ba125d65af049fade56
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 15:47:56 -0400

    ayuda

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -1,5 +1,42 @@
 exports.run = (client, message, args) => {
 
-  message.channel.send("ayudando");
+  message.channel.send({
+
+      embed: {
+      color: 0xa4ff00,
+      author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+      },
+      title: "Enlace Embed",
+      description: "Mensaje de prueba para la descripcion del embed.",
+      fields: [{
+          name: "ban",
+          value: "```Banear a un usuario especificado.```"
+      },
+      {
+          name: "borrar",
+          value: "```Borrar cierta cantidad de publicaciones,requiere permisos.```"
+      },
+      {
+          name: "invitarbot",
+          value: "```Crea una invitacion del bot.```"
+      },
+      {
+          name: "invitar",
+          value: "```Crea una invitacion del servidor.```"
+      },
+      {
+          name: "ayuda",
+          value: "```Muestra los comandos que puedes utilizar.```"
+      }
+      ],
+      timestamp: new Date(),
+          footer: {
+              icon_url: client.user.avatarURL,
+              text: "Bot Oficial del sservidor `xilus Game`",
+          }
+      }
+  });
 
 }
