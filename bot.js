301e76146bdaa17125052039e44dae655658c9fc
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 02:21:21 -0400

    bot

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
+const client = new Discord.Client({disableEveryone: true});
+
+client.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -17,28 +18,27 @@ fs.readdir("./commands/", (err, files) => {
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
+    client.commands.set(props.help.name, props);
   });
 
 });
 
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+client.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${client.guilds.size} servers!`);
 
-  bot.user.setActivity("tutoriales", {type: "WATCHING"});
+  client.user.setActivity("tutoriales", {type: "WATCHING"});
    });
 
-   bot.on("guildMemberAdd", (member) => {
+   client.on("guildMemberAdd", (member) => {
    console.log(`El PODEROSO ${member.user.username} se ha unido a ${member.guild.name}.`);
-   var canal = bot.channels.get('123456789112455845');
+   var canal = client.channels.get('123456789112455845');
    canal.send(`Saludos ${member.user}, bienvenido al servidor, pasala bien.`);
 
-   });
 });
 var prefix = config.prefix;
 
-bot.on("message", (message) =>
+client.on("message", (message) =>
 {
  if (!message.content.startsWith(prefix)) return;
  if (message.author.bot) return;
@@ -46,8 +46,8 @@ bot.on("message", (message) =>
  const args = message.content.slice(prefix.length).trim().split(/ +/g);
  const command = args.shift().toLowerCase();
 
- let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+ let commandfile = client.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(client,message,args);
 
 //   if(command === 'ban'){
 //
