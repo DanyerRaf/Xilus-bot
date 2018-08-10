e3d485ad2ac6bca27da7a542f6a5e22e8b3fe493
DanyerRaf <danyerbmx39@gmail.com>
2018-08-09 22:27:35 -0400

    bot body

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
-const fs = require("fs");
 const  client = new Discord.Client();
+const fs = require("fs");
+const config = require("./config.json");
 
 client.commands = new Discord.Collection();
 
