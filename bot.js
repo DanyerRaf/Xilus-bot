f39e5bd550392c79bedf213a132277dc3e7e7e27
DanyerRaf <danyerbmx39@gmail.com>
2018-08-10 13:39:36 -0400

    bot

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const fs = require("fs");
-const  bot = new Discord.Client();
-
+const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
@@ -23,14 +22,27 @@ fs.readdir("./commands/", (err, files) => {
 
 });
 
-bot.on("ready", () => {
-   console.log("Estoy listo!");
-bot.user.setPresence( {
-       status: "online",
-       game: {
-           name: "Cargando Codigo...",
-           type: "WATCHING"
-       }
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+
+  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+
+  //bot.user.setGame("on SourceCade!");
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
    });
 
    bot.on("guildMemberAdd", (member) => {
@@ -39,7 +51,7 @@ bot.user.setPresence( {
    canal.send(`Saludos ${member.user}, bienvenido al servidor, pasala bien.`);
 
    });
-});
+
 var prefix = config.prefix;
 
 bot.on("message", (message) =>
