1f7627e79f81ae148b2a922c75f92415febfaade
DanyerRaf <danyerbmx39@gmail.com>
2018-08-11 16:25:33 -0400

    add

--- bot.js
+++ bot.js
@@ -18,11 +18,11 @@ client.on("ready", async () => {
 
    client.on('guildMemberAdd', member => {
        let channel = member.guild.channels.find('name', 'welcome-logs')
-       let memberavatar = member.user.iconURL
+       let membericon = member.user.iconURL
        if (!channel) return;
        let embed = new Discord.RichEmbed()
        .setColor('RANDOM')
-       .setThumbnail(memberavatar)
+       .setThumbnail(membericon)
        .addField(':busts_in_silhouette: | Nombre :',`${member}`)
        .addField(':tada: | Mensaje de Bienvenida', `Bienvenido a ${member.guild.name} ${member} nos da gusto verte por aquí.`)
        .addField(':id: | Usuario:', "**["+ ` ${member.id} ` + "]**")
