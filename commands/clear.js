cbf324e3e0762593f9a2dc250f1c42290550b2ea
DanyerRaf <danyerbmx39@gmail.com>
2018-08-11 19:25:55 -0400

    clear

--- commands/clear.js
+++ commands/clear.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
 
 {
-        message.delete (2100);{
+        message.delete (2100);
 
      let messageCount = parseInt(args[0]);
 
@@ -10,12 +10,11 @@ exports.run = (client, message, args) => {
 
      if(messageCount >= 2 && messageCount <= 100){
     message.channel.fetchMessages({limit: messageCount})
-    .then(messages => message.channel.bulkDelete(messages))
+    .then(messages => message.channel.bulkDelete(messages+1))
     .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 
     return message.channel.send('Los mensajes han sido eliminados!').then(m => {m.delete(2150);});
-  }
-   message.delete (1000);
+
      }else{
       return message.reply('El limite a eliminar es de 100 mensajes.');
     }
