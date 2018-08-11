6d2db66ff243575019d230d9925e4fc5e984be94
DanyerRaf <danyerbmx39@gmail.com>
2018-08-11 12:27:58 -0400

    bot

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", async () => {
   console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
 
   // estado = Online,dnd
-  client.user.setStatus("Online");
+  client.user.setStatus("dnd");
 
   // setActivity
   client.user.setActivity("Discord", {type: "WATCHING"});
