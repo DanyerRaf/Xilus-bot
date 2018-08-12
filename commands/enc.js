90fea6e1c22fa7d19e5802b47255e4f73bab9726
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 15:35:21 -0400

    enc

--- commands/enc.js
+++ commands/enc.js
@@ -7,16 +7,16 @@ const Discord = require("discord.js");
     if (!args[0]) return message.channel.send('Proper Usage: <prefijo> enc pregunta');
 
     const embed = new Discord.MessageEmbed()
-    .setColor(0xffffff)
+    .setColor(#65d5ff)
     .setFooter('React to vote')
     .setDescription(args.join(' '))
-    .setTitle(`Enc. creada por ${message.author.username}`);
+    .setTitle(`enc creada por ${message.author.username}`);
 
     let msg = await message.channel.send(embed);
 
     await msg.react(':white_check_mark:');
     await msg.react(':no_entry:');
 
-    message.delete({timeout: 1500});
+    message.delete({timeout: 1000});
 
   }
