ebd96c29095b6e0ba884d80af2071dd77de893bc
DanyerRaf <danyerbmx39@gmail.com>
2018-08-11 22:51:19 -0400

    clear

--- commands/clear.js
+++ commands/clear.js
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
     .then(messages => message.channel.bulkDelete(messages))
     .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 
-    return message.channel.send(messages.size + ' mensajes, borrados...')
+    return message.channel.send(args.size + ' mensajes, borrados...')
     .then(m => {m.delete(2800);});
 
      }else{
