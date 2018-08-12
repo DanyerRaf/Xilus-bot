6491bd62314f8e49a11d578715a8dd1ad4681f06
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 19:14:41 -0400

    clear

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
         message.delete (2100);
 
      let messageCount = parseInt(args[0]);
-     let icon = message.guild.avatarURL('https://cdn.discordapp.com/emojis/469263544198889492.png?v=1')
+     let icon = avatarURL('https://cdn.discordapp.com/emojis/469263544198889492.png?v=1')
      if(!messageCount) return message.reply('Escriba la cantidad a eliminar')
     .then(m => {m.delete(2130);});
 
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
     .then(messages => message.channel.bulkDelete(messages))
     .catch(error => message.reply(`No he podido eliminarlos a causa de: ${error}`));
 
-    return message.channel.send(`He eliminado los mensajes por ti! ${icon}`)
+    return message.channel.send(`He eliminado los mensajes por ti! ${avatarURL}`)
     .then(m => {m.delete(2800);});
      }else{
       return message.reply('El limite a eliminar es de 100 mensajes.');
