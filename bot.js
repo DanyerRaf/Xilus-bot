e16117d7c62c6b5ec24f59c369a1ac27c9327255
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 14:09:16 -0400

    bot

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on("ready", async () => {
   // setActivity
   //client.user.setActivity("atom", {type: "PLAYING"});
 
-  client.user.setGame('atom')//,'https://www.twitch.tv/dafly_1');
+  client.user.setGame('atom');  //,'https://www.twitch.tv/dafly_1');
 
    });
 
