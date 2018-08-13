adfcdd65572f926c849a475e9c9ad0e1d297379d
DanyerRaf <danyerbmx39@gmail.com>
2018-08-13 17:00:25 -0400

    BOT

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on("ready", async () => {
    });
 
    client.on('guildMemberAdd', member => {
-       let channel = member.guild.channels.find('channelID', '477947859828998144' && 'nuevo-invocador')
+       let channel = member.guild.channels.find('name', '🚊nuevo-pasajero' && 'nuevo-invocador')
        let memberavatar = member.user.avatarURL
        if (!channel) return;
        let embed = new Discord.RichEmbed()
