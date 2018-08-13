3fc43664b2da1e0547e957d3594b2ab065747cad
DanyerRaf <danyerbmx39@gmail.com>
2018-08-13 19:24:19 -0400

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,27 @@
 const Discord = require("discord.js");
 
 	exports.run = async (client, message, args,tools) => {
-		let latency = message.channel.guild ?
-			message.channel.guild.shard.latency :
-			bot.shards.get(0).latency;
-		return __("commands.default.ping.success", message, { latency });
+
+    let guild = message.channel.guild;
+		let botCount = guild.members.filter(member => member.user.bot).length;
+		let owner = guild.members.get(guild.ownerID).user;
+
+		return __("commands.default.serverInfo.success", message, {
+			guild: guild.name,
+			channels: guild.channels.size,
+			members: guild.memberCount,
+			users: guild.memberCount - botCount,
+			userPercent: (((guild.memberCount - botCount) / guild.memberCount) * 100).toFixed(2),
+			bots: botCount,
+			botPercent: ((botCount / guild.memberCount) * 100).toFixed(2),
+			creationDate: bot.utils.formatDate(guild.createdAt),
+			owner: `${owner.username}#${owner.discriminator}`,
+			ownerID: owner.id,
+			icon: guild.iconURL ? `<${guild.iconURL}>` : __("words.none", message, {}, true)
+		});
+    
+	guildOnly: true,
+	aliases: ["guildinfo"],
+	description: "Get info about the guild this command was executed in"
 
 };
