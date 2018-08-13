139245648dd06c745811584a45043c7002ec2fe6
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 22:37:05 -0400

    encuestas

--- commands/enc.js
+++ commands/enc.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
   exports.run = async (client, message, args,tools) => {
 
     if (!message.member.roles.find(r => r.name === 'Administrador'))return message.channel.send('Este comando requiere el rol: Administrador')
-    .then(m => {m.delete(2130);});
+    .then(m => {m.delete(3000);});
     if (!message.member.hasPermission('ADMINISTRADOR')) return message.channel.send('Este comando requiere permisos: ADMINISTRADOR');
-
+    .then(m => {m.delete(3000);});
     if (!args[0]) return message.channel.send('Como usar: <prefix> enc pregunta');
 
     let embed = new Discord.RichEmbed()
