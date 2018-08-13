ce9906d32af314295987ea13f566659ca97391f4
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 22:33:56 -0400

    encuesta

--- commands/enc.js
+++ commands/enc.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 
     if (!message.member.roles.find(r => r.name === 'Administrador'))return message.channel.reply('Este comando requiere el rol: Administrador')
     .then(m => {m.delete(2130);});
-    if (!message.member.hasPermission('ADMINISTRADOR')) return message.channel.send('Este comando requiere permisos: ADMINISTRADOR');
+    if (!message.member.hasPermission('ADMINISTRADOR')) return message.channel.reply('Este comando requiere permisos: ADMINISTRADOR');
 
     if (!args[0]) return message.channel.send('Como usar: <prefix> enc pregunta');
 
