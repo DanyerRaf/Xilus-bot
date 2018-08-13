3e31fb058e4cc6858301ef9888e7ac75a3b3d3b2
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 22:39:29 -0400

    banear

--- commands/ban.js
+++ commands/ban.js
@@ -1,14 +1,14 @@
 exports.run = (client, message, args) => {
 
 {
-               message.delete (2100);
+           message.delete (2100);
 
           let user = message.mentions.users.first();
           let razon = args.slice(1).join(' ');
 
-          if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
-          if(!razon) return message.channel.send('Escriba la razón, `>x ban @username [razón]`');
-          if (!message.guild.member(user).bannable) return message.reply('No puedo banear al usuario mencionado.');
+          if (message.mentions.users.size < 1) return message.reply('Debes mencionar a alguien.').catch(console.error);
+          if(!razon) return message.channel.send('Escriba la razón, `>x ban [@username] [razón]`');
+          if (!message.guild.member(user).bannable) return message.reply('No puedes banear al usuario mencionado.');
 
           message.guild.member(user).ban(razon);
           message.channel.send(`**${user.username}**, fue baneado del servidor, razón: ${razon}.`);
