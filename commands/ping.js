8a80eaa390c0f61eef08dd814b3cee5370df9ec1
DanyerRaf <danyerbmx39@gmail.com>
2018-08-13 19:09:12 -0400

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,10 @@
-module.exports = {
-	process: async message => {
+
+	exports.run = async (client, message, args,tools) => {
+    
 		let latency = message.channel.guild ?
 			message.channel.guild.shard.latency :
 			bot.shards.get(0).latency;
 		return __("commands.default.ping.success", message, { latency });
-	},
+
 	description: "Test Oxyl's responsiveness"
 };
