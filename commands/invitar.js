3a410bce943854200db60e790083e1054857d2f0
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 21:57:43 -0400

    invitar

---
+++ commands/invitar.js
@@ -0,0 +1,11 @@
+exports.run = (client, message, args) => {
+
+    message.delete (2000);{
+    const embed = new Discord.RichEmbed()
+    .setTitle("Invocadores Los Invito al Servidor.")
+    .setColor("#f7640e")
+    .setAuthor(message.author.username, message.author.avatarURL);
+    message.channel.send({embed});
+  }
+
+}
