fcbc77e51c5013ddc4e80d529c7e922174b0f9fa
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 22:06:42 -0400

    invitar

--- commands/invitar.js
+++ commands/invitar.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+
 
 exports.run = (client, message, args) => {
 
@@ -9,5 +9,17 @@ exports.run = (client, message, args) => {
     .setAuthor(message.author.username, message.author.avatarURL);
      message.channel.send({embed});
   }
+
+  var id = message.channel.id;
+  message.guild.channels.get(id).createInvite({
+      maxAge: 0  //maxAge: 0 significa que el link sera permanente
+
+  }).then(invite =>
+      message.channel.send("https://discord.gg/BmcaHBc")
+  );
+  /*
+  NOTA: Tambien puede usar maxUse: 1 para el maximo uso del enlace, en vez de maxAge.
+
+  */
 
 }
