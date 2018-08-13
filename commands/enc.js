45a202d6e92a996678f21bb4cd4db222b60c64cf
DanyerRaf <danyerbmx39@gmail.com>
2018-08-12 22:25:43 -0400

    encueata

--- commands/enc.js
+++ commands/enc.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
   exports.run = async (client, message, args,tools) => {
 
     if (!message.member.roles.find(r => r.name === 'Administrador'))return message.channel.send('Este comando requiere el rol: Administrador');
-    .then(m => {m.delete(2130);});
+     message.delete (2100);
     if (!message.member.hasPermission('ADMINISTRADOR')) return message.channel.send('Este comando requiere permisos: ADMINISTRADOR');
 
     if (!args[0]) return message.channel.send('Como usar: <prefix> enc pregunta');
