ebd17eee82be4fc47a5eaa92fe2aab535177f190
DanyerRaf <danyerbmx39@gmail.com>
2018-08-13 19:48:59 -0400

    info

--- commands/infoserver.js
+++ commands/infoserver.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
 
     let sicon = message.guild.iconURL;
-    let owner = guild.members.get(guild.ownerID).user;
     let serverembed = new Discord.RichEmbed()
     .setTitle("Informacion del Servidor")
     .setColor("#15f153")
@@ -12,7 +11,7 @@ exports.run = async (client, message, args) => {
     .addField("Creado:", message.guild.createdAt)
     .addField("Te Uniste:", message.member.joinedAt)
     .addField("Miembros del Servidor:", message.guild.memberCount)
-    .addField("Creador:"`${owner.username}#${owner.discriminator}`);
+    .addField("Creado:", message.owner.ownerID);
     let msg = message.channel.send(serverembed);
 
 };
