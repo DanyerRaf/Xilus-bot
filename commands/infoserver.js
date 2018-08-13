6b71663f0a1e569dcbf23685b8d311574d8fcf6e
DanyerRaf <danyerbmx39@gmail.com>
2018-08-13 19:45:26 -0400

    info

--- commands/infoserver.js
+++ commands/infoserver.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
 
     let sicon = message.guild.iconURL;
+    let owner = guild.members.get(guild.ownerID).user;
     let serverembed = new Discord.RichEmbed()
     .setTitle("Informacion del Servidor")
     .setColor("#15f153")
