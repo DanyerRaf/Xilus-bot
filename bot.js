8437ecc16e518d8bb775055f4f3216b32c212d97
DanyerRaf <danyerbmx39@gmail.com>
2018-10-25 13:52:04 -0400

    bot- nuevo pasajero

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on("ready", async () => {
    });
 
    client.on('guildMemberAdd', member => {
-       let channel = member.guild.channels.find('name', '🚉nuevo-pasajero' && 'nuevo-invocador' && 'bienvenido')
+       let channel = member.guild.channels.find('name', 'nuevo-pasajero' && 'nuevo-invocador' && 'bienvenido')
        let memberavatar = member.user.avatarURL
        if (!channel) return;
        let embed = new Discord.RichEmbed()
